0baa95370719825d5971cb92764cffe97f363f8c
Saumili <haldar.saumili843@gmail.com>
2024-11-13 01:23:07 +0530

    changes 5

--- QuickStore/shop/views.py
+++ QuickStore/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact
 from math import ceil
 
 # Create your views here.
@@ -25,16 +25,26 @@ def about(request):
     return render(request, "shop/about.html")
 
 def contact(request):
-    return HttpResponse("We are at contact")
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, "shop/contact.html")
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request, "shop/tracker.html")
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request, "shop/search.html")
 
-def productView(request):
-    return HttpResponse("We are at product view")
+def productView(request, myid):
+    # Fetching the product using id
+    product=Product.objects.filter(id=myid)
+    print(product)
+    return render(request, "shop/prodView.html", {'product':product[0]})
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request, "shop/checkout.html")
